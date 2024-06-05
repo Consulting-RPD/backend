902d216511f1c73465923f3f013ddedd7be05db8
Salomón Zegarra <salozm34@gmail.com>
2024-06-05 17:32:11 -0500

    Initial commit

---
+++ src/main/java/com/consulting/rpd/RpdApplication.java
@@ -0,0 +1,13 @@
+package com.consulting.rpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpdApplication.class, args);
+	}
+
+}
