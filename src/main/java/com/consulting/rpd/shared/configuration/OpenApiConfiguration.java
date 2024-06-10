8be8c2ebb42267a1dcc77a318b5ddd02dd7e03d9
Salomón Zegarra <salozm34@gmail.com>
2024-06-10 14:39:55 -0500

    chore: add configuration for springdoc

--- src/main/java/com/consulting/rpd/shared/configuration/OpenApiConfiguration.java
+++ src/main/java/com/consulting/rpd/shared/configuration/OpenApiConfiguration.java
@@ -1,9 +1,9 @@
 package com.consulting.rpd.shared.configuration;
 
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,22 +11,22 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfiguration {
     @Bean
     public OpenAPI customOpenApi(){
-        return new OpenAPI()
-                .info(
-                        new Info()
-                                .title("Consulting RPD API")
-                                .version("1.0")
-                                .description("RPD")
-                                .license(
-                                        new License()
-                                                .name("Apache 2.0 License")
-                                                .url("https://essac.com.pe")
-                                )
-                                .contact(
-                                        new Contact()
-                                                .url("https://essac.com.pe")
-                                                .name("Engineering Services & Consulting S.A.C.")
-                                )
-                );
+        return new OpenAPI().info(
+                new Info()
+                        .title("RPD API")
+                        .version("1.0")
+                        .description("RPD API")
+                        .termsOfService("http://www.essac.com.pe")
+                        .license(
+                                new License()
+                                        .name("Apache 2.0")
+                                        .url("http://www.apache.org/licenses/LICENSE-2.0.html")
+                        )
+                        .contact(
+                                new Contact()
+                                        .name("Engineering Services & Consulting S.A.C.")
+                                        .url("https://www.essac.com.pe")
+                        )
+        );
     }
 }
