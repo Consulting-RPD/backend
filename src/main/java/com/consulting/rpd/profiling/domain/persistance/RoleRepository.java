3d72b7c14cc0ef24dec7ce1360db82c71323397b
Salomón Zegarra <salozm34@gmail.com>
2024-06-10 14:44:10 -0500

    refactor: remove unused imports

--- src/main/java/com/consulting/rpd/profiling/domain/persistance/RoleRepository.java
+++ src/main/java/com/consulting/rpd/profiling/domain/persistance/RoleRepository.java
@@ -1,8 +1,6 @@
 package com.consulting.rpd.profiling.domain.persistance;
 
 import com.consulting.rpd.profiling.domain.model.Role;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
