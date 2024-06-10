09e2de991f8b7e620a55dd156fb57507fb0d9b9b
Salomón Zegarra <salozm34@gmail.com>
2024-06-10 18:53:11 -0500

    refactor: remove unused method on role repository contract

--- src/main/java/com/consulting/rpd/profiling/domain/persistance/RoleRepository.java
+++ src/main/java/com/consulting/rpd/profiling/domain/persistance/RoleRepository.java
@@ -11,5 +11,4 @@ import java.util.Optional;
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findById(Long id);
     Optional<Role> findByName(String name);
-    Optional<Role> findByProfileId(Long profileId);
 }
