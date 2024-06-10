fe0e17b9638aaef18e9d64cc95b58fe9baf40bc7
Salomón Zegarra <salozm34@gmail.com>
2024-06-10 14:43:44 -0500

    fix: improve role repository methods

--- src/main/java/com/consulting/rpd/profiling/domain/persistance/RoleRepository.java
+++ src/main/java/com/consulting/rpd/profiling/domain/persistance/RoleRepository.java
@@ -4,12 +4,14 @@ import com.consulting.rpd.profiling.domain.model.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    List<Role> findByRoleId(Long roleId);
-    Page<Role> findByRoleId(Long roleId, Pageable pageable);
+    Optional<Role> findById(Long id);
     Optional<Role> findByName(String name);
+    Optional<Role> findByProfileId(Long profileId);
 }
