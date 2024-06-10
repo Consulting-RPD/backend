9c0a8612c5490b05019385124b9b9a4e1c29884c
Salomón Zegarra <salozm34@gmail.com>
2024-06-10 17:59:26 -0500

    feat: added role resource

---
+++ src/main/java/com/consulting/rpd/profiling/resource/RoleResource.java
@@ -0,0 +1,13 @@
+package com.consulting.rpd.profiling.resource;
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleResource {
+    private Long id;
+    private String name;
+}
