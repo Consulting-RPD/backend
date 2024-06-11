e60a4902381170537381922799fa02998d59214c
Salomón Zegarra <salozm34@gmail.com>
2024-06-11 15:43:50 -0500

    feat: added Client service interface

---
+++ src/main/java/com/consulting/rpd/projects/domain/service/ClientService.java
@@ -0,0 +1,17 @@
+package com.consulting.rpd.projects.domain.service;
+
+import com.consulting.rpd.projects.domain.model.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface ClientService {
+    List<Client> getAll();
+    Page<Client> getAll(Pageable pageable);
+    Client getById(Long clientId);
+    Client create(Client client);
+    Client update(Client client);
+    ResponseEntity<?> delete(Long clientId);
+}
