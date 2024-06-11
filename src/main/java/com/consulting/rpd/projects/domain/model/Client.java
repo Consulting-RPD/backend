b10583e7d1df64e003708094f542ce8dd6dec463
Salomón Zegarra <salozm34@gmail.com>
2024-06-11 15:43:29 -0500

    feat: added Client model

---
+++ src/main/java/com/consulting/rpd/projects/domain/model/Client.java
@@ -0,0 +1,49 @@
+package com.consulting.rpd.projects.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String tradeName;
+
+    @NotNull
+    @NotBlank
+    private String legalName;
+
+    @NotNull
+    @NotBlank
+    private Integer ruc;
+
+    @NotNull
+    @NotBlank
+    private Date registrationDate;
+
+    @NotNull
+    @NotBlank
+    private String city;
+
+    @NotNull
+    @NotBlank
+    private String country;
+
+    @NotNull
+    @NotBlank
+    private String clientCode;
+}
