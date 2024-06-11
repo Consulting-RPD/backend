8ada1eb0f35335f6f859ee8d9275ba9f5ea4a1d3
Salomón Zegarra <salozm34@gmail.com>
2024-06-11 17:25:47 -0500

    feat: added project model

---
+++ src/main/java/com/consulting/rpd/projects/domain/model/Project.java
@@ -0,0 +1,19 @@
+package com.consulting.rpd.projects.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
