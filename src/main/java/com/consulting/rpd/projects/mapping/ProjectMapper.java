765b6362ddae2c07fe7b34aa126df4981f8fe67d
Salomón Zegarra <salozm34@gmail.com>
2024-06-12 12:03:50 -0500

    feat: added Project mapper

---
+++ src/main/java/com/consulting/rpd/projects/mapping/ProjectMapper.java
@@ -0,0 +1,28 @@
+package com.consulting.rpd.projects.mapping;
+
+import com.consulting.rpd.projects.domain.model.Project;
+import com.consulting.rpd.projects.resource.CreateProjectResource;
+import com.consulting.rpd.projects.resource.ProjectResource;
+import com.consulting.rpd.projects.resource.UpdateProjectResource;
+import com.consulting.rpd.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class ProjectMapper {
+    @Autowired
+    private EnhancedModelMapper mapper;
+
+    public ProjectResource toResource(Project model) {return mapper.map(model, ProjectResource.class);}
+
+    public Page<ProjectResource> modelListPage(List<Project> modelList, Pageable pageable){
+        return new PageImpl<>(mapper.mapList(modelList, ProjectResource.class), pageable, modelList.size());
+    }
+
+    public Project toModel(CreateProjectResource resource) {return mapper.map(resource, Project.class);}
+
+    public Project toModel(UpdateProjectResource resource) {return mapper.map(resource, Project.class);}
+}
