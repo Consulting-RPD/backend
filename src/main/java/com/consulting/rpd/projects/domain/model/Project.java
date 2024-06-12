54e20a23de6385b469e84508d8b223a3cd1224bf
Salomón Zegarra <salozm34@gmail.com>
2024-06-12 11:38:20 -0500

    feat: added relationship to model

--- src/main/java/com/consulting/rpd/projects/domain/model/Project.java
+++ src/main/java/com/consulting/rpd/projects/domain/model/Project.java
@@ -1,9 +1,12 @@
 package com.consulting.rpd.projects.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -16,4 +19,26 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @NotBlank
+    private String projectCode;
+
+    @NotNull
+    @NotBlank
+    private String projectCorrelative;
+
+    @NotNull
+    @NotBlank
+    private Date registrationDate;
+
+    @NotNull
+    @NotBlank
+    private String projectName;
+
+    //Relationships
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "client_id", nullable = false)
+    @JsonIgnore
+    private Client client;
 }
