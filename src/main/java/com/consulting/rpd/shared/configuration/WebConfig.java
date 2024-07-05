c4da8f99689b7950a31a61af31b8555d4ce9267e
Salomón Zegarra <salozm34@gmail.com>
2024-07-05 15:25:40 -0500

    feat: added cors configuration

---
+++ src/main/java/com/consulting/rpd/shared/configuration/WebConfig.java
@@ -0,0 +1,19 @@
+package com.consulting.rpd.shared.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+      .allowedOrigins("http://localhost:3000", "https://rpd-consulting.netlify.app/")
+      .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+      .allowedHeaders("*")
+      .allowCredentials(true);
+    }
+}
